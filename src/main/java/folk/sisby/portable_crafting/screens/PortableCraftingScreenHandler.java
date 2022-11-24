6e66b6a6ad1ce880a1903b7bd905df1bad092daa
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-11-24 13:10:31 +1100

    Update to 1.19

--- src/main/java/folk/sisby/portable_crafting/screens/PortableCraftingScreenHandler.java
+++ src/main/java/folk/sisby/portable_crafting/screens/PortableCraftingScreenHandler.java
@@ -7,12 +7,11 @@ import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.registry.Registry;
 
 public class PortableCraftingScreenHandler extends CraftingScreenHandler {
 	public static final ScreenHandlerType<PortableCraftingScreenHandler> PORTABLE_CRAFTING_HANDLER_TYPE = Registry.register(Registry.SCREEN_HANDLER, PortableCrafting.ID, new ScreenHandlerType<>(PortableCraftingScreenHandler::new));
-	public static final Text LABEL = new TranslatableText("container.portable_crafting.portable_crafting_table");
+	public static final Text LABEL = Text.translatable("container.portable_crafting.portable_crafting_table");
 
 
 	public PortableCraftingScreenHandler(int syncId, PlayerInventory playerInventory) {
