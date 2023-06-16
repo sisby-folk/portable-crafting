1c8f6c907d584e2178d0c420e1ddf26880395c4d
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-17 00:25:45 +1000

    update to 1.19.4

--- src/main/java/folk/sisby/portable_crafting/screens/PortableCraftingScreenHandler.java
+++ src/main/java/folk/sisby/portable_crafting/screens/PortableCraftingScreenHandler.java
@@ -3,6 +3,7 @@ package folk.sisby.portable_crafting.screens;
 import folk.sisby.portable_crafting.PortableCrafting;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.feature_flags.FeatureFlagBitSet;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.screen.CraftingScreenHandler;
@@ -11,7 +12,7 @@ import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.text.Text;
 
 public class PortableCraftingScreenHandler extends CraftingScreenHandler {
-	public static final ScreenHandlerType<PortableCraftingScreenHandler> PORTABLE_CRAFTING_HANDLER_TYPE = Registry.register(Registries.SCREEN_HANDLER_TYPE, PortableCrafting.ID, new ScreenHandlerType<>(PortableCraftingScreenHandler::new));
+	public static final ScreenHandlerType<PortableCraftingScreenHandler> PORTABLE_CRAFTING_HANDLER_TYPE = Registry.register(Registries.SCREEN_HANDLER_TYPE, PortableCrafting.ID, new ScreenHandlerType<>(PortableCraftingScreenHandler::new, FeatureFlagBitSet.empty()));
 	public static final Text LABEL = Text.translatable("container.portable_crafting.portable_crafting_table");
 
 	public PortableCraftingScreenHandler(int syncId, PlayerInventory playerInventory) {
