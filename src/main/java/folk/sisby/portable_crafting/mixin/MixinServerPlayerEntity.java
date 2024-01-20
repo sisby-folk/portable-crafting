1695f6766490f80f9294e65c4caaee6066192027
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-01-20 22:10:39 +1100

    click & desync fix

--- src/main/java/folk/sisby/portable_crafting/mixin/MixinServerPlayerEntity.java
+++ src/main/java/folk/sisby/portable_crafting/mixin/MixinServerPlayerEntity.java
@@ -3,6 +3,7 @@ package folk.sisby.portable_crafting.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import folk.sisby.portable_crafting.PortableCrafting;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,6 +18,6 @@ public class MixinServerPlayerEntity {
 
 	@WrapWithCondition(method = "openHandledScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;closeHandledScreen()V"))
 	private boolean dontCloseFromPortableScreen(ServerPlayerEntity instance) {
-		return !PortableCrafting.CHANGING_SCREENS;
+		return !ServerPlayNetworking.canSend(instance, PortableCrafting.S2C_SCREENS_ENABLED) || !PortableCrafting.CHANGING_SCREENS;
 	}
 }
